ce556d4320f1c86692400cbeea7e191d328e709f
= <=>
2015-03-12 15:53:36 +0800

    增加异常类

---
+++ src/com/Pic/exception/FileExistException.java
@@ -0,0 +1,30 @@
+package com.Pic.exception;
+
+public class FileExistException extends RuntimeException {
+
+	public FileExistException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public FileExistException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public FileExistException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public FileExistException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public FileExistException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/com/Pic/exception/FileNotFoundExceotion.java
@@ -0,0 +1,32 @@
+package com.Pic.exception;
+
+public class FileNotFoundExceotion extends RuntimeException {
+
+	public FileNotFoundExceotion() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public FileNotFoundExceotion(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public FileNotFoundExceotion(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public FileNotFoundExceotion(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public FileNotFoundExceotion(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
