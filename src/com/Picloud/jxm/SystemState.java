943d639340848549dd22c20a5111bcced17e3298
sunzequn <sunzequn@live.cn>
2015-04-06 17:36:21 +0800

    增加监控

---
+++ src/com/Picloud/jxm/SystemState.java
@@ -0,0 +1,86 @@
+package com.Picloud.jxm;
+
+import java.text.DecimalFormat;
+
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+import com.Picloud.web.model.SystemStateInfo;
+
+public class SystemState {
+
+	private static String nameNodeInfo = "http://localhost:50070/jmx?qry=Hadoop:service=NameNode,name=NameNodeInfo";
+	private static String nameNodeStatus = "http://localhost:50070/jmx?qry=Hadoop:service=NameNode,name=NameNodeStatus";
+	private static String FSNamesystemState = "http://localhost:50070/jmx?qry=Hadoop:service=NameNode,name=FSNamesystemState";
+	private static String memory = "http://localhost:50070/jmx?qry=java.lang:type=Memory";
+	private static DecimalFormat df = new DecimalFormat("#.00");
+
+	public static SystemStateInfo getSystemState() {
+		SystemStateInfo info = new SystemStateInfo();
+
+		JSONObject jsonNameNodeInfo = new JSONObject().fromObject(SendGet
+				.sendGet(nameNodeInfo));
+		JSONArray jsonBeans = jsonNameNodeInfo.getJSONArray("beans");
+		JSONObject jsonInfo = jsonBeans.getJSONObject(0);
+
+		double used = Double.valueOf(jsonInfo.getString("Used"))
+				/ (1024 * 1024);
+		info.setDfsUsed(df.format(used) + "MB");
+
+		double blockPoolUsed = Double.valueOf(jsonInfo
+				.getString("BlockPoolUsedSpace")) / (1024 * 1024);
+		info.setBlockPoolUsed(df.format(blockPoolUsed) + "MB");
+
+		double nonUsed = Double.valueOf(jsonInfo.getString("NonDfsUsedSpace"))
+				/ (1024 * 1024 * 1024);
+		info.setNonDFSUsed(df.format(nonUsed) + "GB");
+
+		double dfsRemaining = Double.valueOf(jsonInfo.getString("Free"))
+				/ (1024 * 1024 * 1024);
+		info.setDfsRemaining(df.format(dfsRemaining) + "GB");
+
+		double percentUsed = Double.valueOf(jsonInfo.getString("PercentUsed"));
+		if (percentUsed < 1.0)
+			info.setDfsUsedPercent("0" + df.format(percentUsed) + "%");
+		else
+			info.setDfsUsedPercent(df.format(percentUsed) + "%");
+
+		double percentRemaining = Double.valueOf(jsonInfo
+				.getString("PercentRemaining"));
+		if (percentRemaining < 1.0)
+			info.setDfsRemainingPercent("0" + df.format(percentRemaining) + "%");
+		else
+			info.setDfsRemainingPercent(df.format(percentRemaining) + "%");
+
+		double blockPoolUsedPercent = Double.valueOf(jsonInfo
+				.getString("PercentBlockPoolUsed"));
+		if (blockPoolUsedPercent < 1.0)
+			info.setBlockPoolUsedPercent("0" + df.format(blockPoolUsedPercent)
+					+ "%");
+		else
+			info.setBlockPoolUsedPercent(df.format(blockPoolUsedPercent) + "%");
+
+		JSONObject jsonNode = jsonInfo.getJSONObject("NodeUsage")
+				.getJSONObject("nodeUsage");
+		String nodeUsageMin = jsonNode.getString("min");
+		info.setNodeUsageMin(nodeUsageMin);
+
+		String nodeUsageMedian = jsonNode.getString("median");
+		info.setNodeUsageMedian(nodeUsageMedian);
+
+		String nodeUsageMax = jsonNode.getString("max");
+		info.setNodeUsageMax(nodeUsageMax);
+
+		String nodeUsageStdDev = jsonNode.getString("stdDev");
+		info.setNodeUsageStdDev(nodeUsageStdDev);
+
+		System.out.println(info);
+		return info;
+	}
+//
+//	public static void main(String[] args) {
+//		getSystemState();
+//	}
+
+}
---
+++ src/com/Picloud/web/model/SystemStateInfo.java
@@ -0,0 +1,97 @@
+package com.Picloud.web.model;
+
+public class SystemStateInfo {
+
+	private String dfsUsed;
+	private String nonDFSUsed;
+	private String dfsRemaining;
+	private String dfsUsedPercent;
+	private String dfsRemainingPercent;
+	private String blockPoolUsed;
+	private String blockPoolUsedPercent;
+	private String nodeUsageMin;
+	private String nodeUsageMedian;
+	private String nodeUsageMax;
+	private String nodeUsageStdDev;
+	
+	
+	public String getNodeUsageMin() {
+		return nodeUsageMin;
+	}
+	public void setNodeUsageMin(String nodeUsageMin) {
+		this.nodeUsageMin = nodeUsageMin;
+	}
+	public String getNodeUsageMedian() {
+		return nodeUsageMedian;
+	}
+	public void setNodeUsageMedian(String nodeUsageMedian) {
+		this.nodeUsageMedian = nodeUsageMedian;
+	}
+	public String getNodeUsageMax() {
+		return nodeUsageMax;
+	}
+	public void setNodeUsageMax(String nodeUsageMax) {
+		this.nodeUsageMax = nodeUsageMax;
+	}
+	public String getNodeUsageStdDev() {
+		return nodeUsageStdDev;
+	}
+	public void setNodeUsageStdDev(String nodeUsageStdDev) {
+		this.nodeUsageStdDev = nodeUsageStdDev;
+	}
+	public String getDfsUsed() {
+		return dfsUsed;
+	}
+	public void setDfsUsed(String dfsUsed) {
+		this.dfsUsed = dfsUsed;
+	}
+	public String getNonDFSUsed() {
+		return nonDFSUsed;
+	}
+	public void setNonDFSUsed(String nonDFSUsed) {
+		this.nonDFSUsed = nonDFSUsed;
+	}
+	public String getDfsRemaining() {
+		return dfsRemaining;
+	}
+	public void setDfsRemaining(String dfsRemaining) {
+		this.dfsRemaining = dfsRemaining;
+	}
+	public String getDfsUsedPercent() {
+		return dfsUsedPercent;
+	}
+	public void setDfsUsedPercent(String dfsUsedPercent) {
+		this.dfsUsedPercent = dfsUsedPercent;
+	}
+	public String getDfsRemainingPercent() {
+		return dfsRemainingPercent;
+	}
+	public void setDfsRemainingPercent(String dfsRemainingPercent) {
+		this.dfsRemainingPercent = dfsRemainingPercent;
+	}
+	public String getBlockPoolUsed() {
+		return blockPoolUsed;
+	}
+	public void setBlockPoolUsed(String blockPoolUsed) {
+		this.blockPoolUsed = blockPoolUsed;
+	}
+	public String getBlockPoolUsedPercent() {
+		return blockPoolUsedPercent;
+	}
+	public void setBlockPoolUsedPercent(String blockPoolUsedPercent) {
+		this.blockPoolUsedPercent = blockPoolUsedPercent;
+	}
+	@Override
+	public String toString() {
+		return "SystemStateInfo [dfsUsed=" + dfsUsed + ", nonDFSUsed="
+				+ nonDFSUsed + ", dfsRemaining=" + dfsRemaining
+				+ ", dfsUsedPercent=" + dfsUsedPercent
+				+ ", dfsRemainingPercent=" + dfsRemainingPercent
+				+ ", blockPoolUsed=" + blockPoolUsed
+				+ ", blockPoolUsedPercent=" + blockPoolUsedPercent
+				+ ", nodeUsageMin=" + nodeUsageMin + ", nodeUsageMedian="
+				+ nodeUsageMedian + ", nodeUsageMax=" + nodeUsageMax
+				+ ", nodeUsageStdDev=" + nodeUsageStdDev + "]";
+	}
+	
+}
